3f54ca4f620d89dc098abc9552d4bf49e84fa552
Young Joon Lee <yj.lee@chu.ac.kr>
2022-07-22 11:15:15 +0900

    docker run: use host network, remove port mapping

--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -9,11 +9,13 @@ CMD=${2:-/bin/bash}
 
 docker run -it --rm \
   --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=$NV_VISIBLE_DEVICES \
+  --network=host \
   --ipc=host \
   --shm-size=$DOCKER_SHM_SIZE \
   --ulimit memlock=-1 \
   --ulimit stack=67108864 \
-  -p $JUPYTER_HOST_PORT:$JUPYTER_PORT \
   -e WANDB_API_KEY=$WANDB_API_KEY \
   -v $WORKSPACE_HOST:$WORKSPACE_DOCKER \
   $DISCO_IMAGEN_DOCKER $CMD
+
+  # -p $JUPYTER_HOST_PORT:$JUPYTER_PORT \

2a2a0886f3e36d8b967f81a90b7e839f85ff941f
Young Joon Lee <yj.lee@chu.ac.kr>
2022-07-22 13:16:08 +0900

    update setup script

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # disco-imagen
 
 sudo curl -s https://raw.githubusercontent.com/entelecheia/disco-imagen/main/scripts/.setup.sh | bash
+sudo bash ./scripts/docker/build.sh
+sudo bash ./scripts/docker/jupyter.sh
--- scripts/.setup.sh
+++ scripts/.setup.sh
@@ -4,5 +4,3 @@ mkdir -p ~/workspace/projects
 cd ~/workspace/projects
 git clone https://github.com/entelecheia/disco-imagen.git
 cd ~/workspace/projects/disco-imagen
-source ./scripts/docker/build.sh
-source ./scripts/docker/jupyter.sh

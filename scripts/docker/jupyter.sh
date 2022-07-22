7814e6bb29184cee3819984ecf00bac1a004b3c9
Young Joon Lee <yj.lee@chu.ac.kr>
2022-07-22 13:50:54 +0900

    update dockerfile

--- scripts/docker/jupyter.sh
+++ scripts/docker/jupyter.sh
@@ -15,7 +15,6 @@ docker run -itd --rm \
   --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=$NV_VISIBLE_DEVICES \
   --network=host \
   --ipc=host \
-  --shm-size=$DOCKER_SHM_SIZE \
   --ulimit memlock=-1 \
   --ulimit stack=67108864 \
   -e WANDB_API_KEY=$WANDB_API_KEY \
--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -11,7 +11,6 @@ docker run -it --rm \
   --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=$NV_VISIBLE_DEVICES \
   --network=host \
   --ipc=host \
-  --shm-size=$DOCKER_SHM_SIZE \
   --ulimit memlock=-1 \
   --ulimit stack=67108864 \
   -e WANDB_API_KEY=$WANDB_API_KEY \

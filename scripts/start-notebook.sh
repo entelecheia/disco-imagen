f9384472d9a3e22a5b6953de57208e5dd10f1a72
Young Joon Lee <yj.lee@chu.ac.kr>
2022-07-23 17:11:14 +0900

    Update start-notebook.sh

--- scripts/start-notebook.sh
+++ scripts/start-notebook.sh
@@ -4,13 +4,8 @@ set -o allexport
 source .env
 set +o allexport
 
-mkdir -p $JUPYTER_LOG_DIR
-DATESTAMP=$(date +'%y%m%d%H%M%S')
-LOGFILE=$JUPYTER_LOG_DIR/.jupyter-$DATESTAMP.log
-printf "Logs written to %s\n" "$LOGFILE"
-
 jupyter notebook \
     --no-browser -NotebookApp.token=$JUPYTER_TOKEN \
     --notebook-dir=$EKORPKIT_PROJECT_DIR \
     --port=$JUPYTER_PORT --ip=0.0.0.0 \
-    --allow-root &> $LOGFILE &
+    --allow-root

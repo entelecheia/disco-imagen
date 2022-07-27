ec1c11bd108261678b1e449c603fbc1ef0cde575
Young Joon Lee <yj.lee@chu.ac.kr>
2022-07-27 20:05:59 +0900

    update docker scripts

--- scripts/docker/jupyter.sh
+++ scripts/docker/jupyter.sh
@@ -5,8 +5,8 @@ source .env
 set +o allexport
 
 if [ "$HOST_WORKSPACE_ROOT" == "" ]; then
-    HOST_WORKSPACE_ROOT=${HOME}${EKORPKIT_WORKSPACE_ROOT}
-    echo "HOST_WORKSPACE_ROOT is empty, using: $HOST_WORKSPACE_ROOT"
+  HOST_WORKSPACE_ROOT=${HOME}${EKORPKIT_WORKSPACE_ROOT}
+  echo "HOST_WORKSPACE_ROOT is empty, using: $HOST_WORKSPACE_ROOT"
 fi
 
 docker run -itd --rm \
--- scripts/docker/run.sh
+++ scripts/docker/run.sh
@@ -7,8 +7,8 @@ set +o allexport
 CMD=${1:-/bin/bash}
 
 if [ "$HOST_WORKSPACE_ROOT" == "" ]; then
-    HOST_WORKSPACE_ROOT=${HOME}${EKORPKIT_WORKSPACE_ROOT}
-    echo "HOST_WORKSPACE_ROOT is empty, using: $HOST_WORKSPACE_ROOT"
+  HOST_WORKSPACE_ROOT=${HOME}${EKORPKIT_WORKSPACE_ROOT}
+  echo "HOST_WORKSPACE_ROOT is empty, using: $HOST_WORKSPACE_ROOT"
 fi
 
 docker run -itd --rm \
